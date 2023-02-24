c5ffe7e25d2bc83968d1eb7892807f7209d46159
sergey.tolochny <tolochnuys@gmail.com>
2023-02-24 10:15:28 +0200

    Add readme notes

--- P04/src/test/java/Settings/WebDriverSettings.java
+++ P04/src/test/java/Settings/WebDriverSettings.java
@@ -3,8 +3,9 @@ package Settings;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 
 import java.time.Duration;
 
@@ -13,14 +14,14 @@ public class WebDriverSettings {
         public WebDriver driver;
         public WebDriverWait wait;
 
-        @BeforeTest
+        @BeforeClass
         public void setUp() {
             System.setProperty("webdriver.chrome.driver","C:\\Program Files\\DriversSelenium\\chromedriver.exe");
             driver = new ChromeDriver();
             wait=new WebDriverWait(driver, Duration.ofSeconds(10));
         }
 
-        @AfterTest
+        @AfterClass
         public void close() {
             driver.close();
             driver.quit();
